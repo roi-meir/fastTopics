6ac14532f85cfac82041d42f5a566e6a3343f6a0
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-04-17 08:51:17 -0500

    Added another test for project_poisson_nmf().

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -4,7 +4,7 @@ test_that("project_poisson_nmf leaves the F matrix unchanged",{
 
   # Simulate a 160 x 100 counts matrix.
   set.seed(1)
-  dat   <- simulate_multinom_gene_data(160,100,k = 3)
+  dat   <- simulate_count_data(160,100,k = 3)
   train <- dat$X[1:80,]
   test  <- dat$X[81:160,]
 
@@ -18,4 +18,18 @@ test_that("project_poisson_nmf leaves the F matrix unchanged",{
   # Predict L in unseen (test) data points.
   capture.output(out <- project_poisson_nmf(test,F,numiter = 20))
   expect_equal(rep(1,3),unname(diag(cor(out$F,F))),tolerance = 1e-8)
+})
+
+test_that(paste("Running project_poisson_nmf on the training data",
+                "should produce the same L matrix"),{
+
+  # Simulate a 80 x 100 counts matrix.
+  set.seed(1)
+  X <- simulate_count_data(80,100,k = 3)$X
+
+  # Fit a Poisson non-negative matrix factorization to X.
+  capture.output(fit <- init_poisson_nmf(X,F = dat$F,init.method = "random"))
+  capture.output(fit <- fit_poisson_nmf(X,fit0 = fit))
+  capture.output(out <- project_poisson_nmf(X,fit$F,numiter = 20))
+  expect_equal(rep(1,3),unname(diag(cor(out$L,fit$L))),tolerance = 1e-3)
 })
