7d43b89a5f51223a05192a115741072cde09ec2b
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-04-17 08:35:57 -0500

    A few fixes to the tests in test_predict.R.

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -1,20 +1,21 @@
 context("predict")
 
-# Test that project_poisson_nmf returns F with the same structure as the input F
-test_that("project_poisson_nmf returns F with the same structure as the input F", {
+test_that("project_poisson_nmf leaves the F matrix unchanged",{
 
+  # Simulate a 160 x 100 counts matrix.
   set.seed(1)
-  dat   <- simulate_multinom_gene_data(175,1200,k = 3)
-  train <- dat$X[1:100,]
-  test  <- dat$X[101:175,]
+  dat   <- simulate_multinom_gene_data(160,100,k = 3)
+  train <- dat$X[1:80,]
+  test  <- dat$X[81:160,]
 
-  fit    <- init_poisson_nmf(train,F = dat$F,init.method = "random")
-  fit    <- fit_poisson_nmf(train,fit0 = fit,verbose = "none")
-  fit    <- poisson2multinom(fit)
-  F_pois <- multinom2poisson(fit)$F
+  # Fit a Poisson non-negative matrix factorization using the
+  # training data.
+  capture.output(fit <- init_poisson_nmf(train,F = dat$F,
+                                         init.method = "random"))
+  capture.output(fit <- fit_poisson_nmf(train,fit0 = fit))
+  F <- fit$F
 
-  out <- project_poisson_nmf(test,F_pois,numiter = 20,verbose = "none", update.factors = NULL)
-
-  norm_col <- function(M) sweep(M,2,colSums(M),"/")
-  expect_equal(norm_col(out$F),norm_col(F_pois),tolerance = 1e-10)
+  # Predict L in unseen (test) data points.
+  capture.output(out <- project_poisson_nmf(test,F,numiter = 20))
+  expect_equal(rep(1,3),unname(diag(cor(out$F,F))),tolerance = 1e-8)
 })
